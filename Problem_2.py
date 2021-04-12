830a7b66d19427d2a7ac540e48b704e52e200a15
mohammedegila <mohammedegila@gmail.com>
2021-04-12 09:54:34 +0200

    remove duplicates from given list

---
+++ Problem_2.py
@@ -0,0 +1,16 @@
+ 
+#Problem #2 remove duplicates from given list 
+def unique_list(elements):
+  '''
+  Function to remove duplicates from given list 
+  '''
+  single_list = []
+  for element in elements:
+    if element not in single_list:
+      single_list.append(element)
+  return single_list
+
+elements = [7, 7, 8, 8, 5]
+print("List Before:  " , elements)
+print("============================") 
+print("List After:  " , unique_list(elements))
